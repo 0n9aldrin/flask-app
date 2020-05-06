974ea757532f838aa02e7e32fa8b095c8d077b36
0n9aldrin <0n9aldrin@gmail.com>
2020-05-06 11:37:26 +0700

    tets3

--- app.py
+++ app.py
@@ -7,14 +7,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    driver.get('https://google.com')
-    return driver.page_source
+    return 'hello'
+    # chrome_options = webdriver.ChromeOptions()
+    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    # chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--disable-dev-shm-usage")
+    # chrome_options.add_argument("--no-sandbox")
+    # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    # driver.get('https://google.com')
+    # return driver.page_source
 
 
 if __name__ == "__main__":
