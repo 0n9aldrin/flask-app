d3f8b835b06289bb8432dc08b7c25079983c3713
0n9aldrin <0n9aldrin@gmail.com>
2020-05-06 20:52:37 +0700

    test12

--- app.py
+++ app.py
@@ -14,23 +14,33 @@ app = Flask(__name__)
 def index():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--proxy-server='direct://'")
+    chrome_options.add_argument("--proxy-bypass-list=*")
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--headless")
+    prefs = {'profile.managed_default_content_settings.images': 2, 'useAutomationExtension': False}
+    chrome_options.add_experimental_option("prefs", prefs)
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get("https://shopee.co.id/search?keyword=calculator")
     actions = ActionChains(driver)
 
-    for x in range(7):
+   for x in range(7):
         actions.send_keys(Keys.PAGE_DOWN).perform()
+        time.sleep(0.005)
 
-    name = driver.find_elements_by_css_selector("#main > div > div.shopee-page-wrapper > div.container._2_Y1cV > div.jrLh5s > div.shopee-search-item-result > div.row.shopee-search-item-result__items > div > div > a > div > div:nth-child(2) > div:nth-child(1) > div")
+    name = driver.find_elements_by_css_selector(
+        "#main > div > div.shopee-page-wrapper > div.container._2_Y1cV > div.jrLh5s > div.shopee-search-item-result > div.row.shopee-search-item-result__items > div > div > a > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)")
 
     names = ''
     for x in range(len(name)):
-        names = names + name[x].text
-    names = names + str(len(name))
-    
+        names = names + name[x].text + '\n'
+    names = names + ' ' + str(len(name))
+
     return names
 
 
